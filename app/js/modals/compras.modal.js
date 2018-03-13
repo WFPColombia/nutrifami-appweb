cc81252e1c888d88e55fe25d574b28f37f7de4dd
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2018-03-13 15:21:45 -0500

    Reorder of some files

--- app/js/modals/compras.modal.js
+++
@@ -1,8 +0,0 @@
-nf2.controller('ComprasGrupoModalController', function($scope, $uibModalInstance, $timeout, data, ngAudio) {
-    $scope.data = data;
-
-    $scope.playAudio = function(grupo_id) {
-        var audio = ngAudio.load('audios/compras-resumen-' + grupo_id + '.mp3');
-        audio.play();
-    };
-});
